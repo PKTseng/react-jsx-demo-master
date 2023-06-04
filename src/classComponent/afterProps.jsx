c44595b88bc22f53a4b6f45c7d7501fb33ebcd28
PKTseng <jk0910948659@gmail.com>
2023-06-04 14:39:29 +0800

    update

--- src/classComponent/afterProps.jsx
+++ src/classComponent/afterProps.jsx
@@ -8,20 +8,26 @@ function SonF(props) {
 
 class SonC extends React.Component {
   render() {
-    return <h2>Son {this.props.message}</h2>
+    return (
+      <>
+        {/* <h2>Son {this.props.message}</h2> */}
+        <div>Son List {this.props.list}</div>
+      </>
+    )
   }
 }
 
 class App extends React.Component {
   state = {
-    message: 'this is message',
+    // message: 'this is message',
+    list: [1, 2, 3],
   }
 
   render() {
     return (
       <>
-        <SonF message={this.state.message} />
-        <SonC message={this.state.message} />
+        {/* <SonF message={this.state.message} /> */}
+        <SonC list={this.state.list} />
       </>
     )
   }
